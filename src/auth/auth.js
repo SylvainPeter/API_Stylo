5183fba56c8c94e2cc2484a2000fd437b8f241d3
sylvainpeter <lexriders@gmail.com>
2020-12-12 10:07:00 +0100

    Ajoute les formulaires

---
+++ src/auth/auth.js
@@ -0,0 +1,80 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const UserModel = require('../model/model');
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+/* saves the information provided by the user to the database, 
+and then sends the user information to the next middleware if successful.
+*/
+passport.use(
+  'signup',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.create({ email, password });
+
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+
+// finds one user associated with the email provided
+passport.use(
+  'login',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+          return done(null, false, { message: 'User not found' });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: 'Wrong Password' });
+        }
+
+        return done(null, user, { message: 'Logged in Successfully' });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+
+/* extract the JWT from the query parameter. 
+It then verifies that this token has been signed with the secret or key set during logging in (TOP_SECRET). 
+If the token is valid, the user details are passed to the next middleware.
+*/
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: 'TOP_SECRET',
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
